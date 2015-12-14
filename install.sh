7039163cc55691ab5b4526cf817cbba31e5aabb3
Yuce Tekol <yucetekol@gmail.com>
2015-12-14 02:39:22 +0200

    Added .vimrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 ROOT=`pwd`
 BIN_DIR=$HOME/bin
 
+# === install .bashrc ===
 if [ -f $HOME/.bashrc ]
 then
     mv $HOME/.bashrc $HOME/OLD.bashrc
@@ -12,4 +13,11 @@ if [ ! -f $BIN_DIR/z.sh ]
 then
     ln -s $ROOT/bin/z.sh $BIN_DIR/z.sh
 fi
+
+# === install .vimrc ===
+if [ -f $HOME/.vimrc ]
+then
+    mv $HOME/.vimrc $HOME/OLD.vimrc
+fi
+ln -s $ROOT/editor/_vimrc $HOME/.vimrc
 
