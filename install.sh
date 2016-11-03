1ecf20bcbfd2672953439d4e91262dc9e53d3fac
Yuce Tekol <yucetekol@gmail.com>
2016-11-04 00:51:15 +0300

    added i3 config

--- install.sh
+++ install.sh
@@ -21,3 +21,22 @@ then
 fi
 ln -s $ROOT/editor/_vimrc $HOME/.vimrc
 
+# === install i3 config ===
+i3_config_dir=$HOME/.config/i3
+i3_config_path=$i3_config_dir/config
+if [ -f $i3_config_path ]
+then
+    mv $i3_config_path OLD.i3_config_path
+fi
+mkdir -p $i3_config_dir
+ln -s  $ROOT/wm/i3/config $i3_config_path
+
+i3blocks_config_dir=$HOME/.i3
+i3blocks_config_path=$i3blocks_config_dir/i3blocks.conf
+if [ -f $i3blocks_config_path ]
+then
+    mv $i3blocks_config_path $i3blocks_config_path.OLD
+fi
+mkdir -p $i3blocks_config_dir
+ln -s $ROOT/wm/i3/i3blocks.conf $i3blocks_config_path
+
