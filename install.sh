0cbe01e2f84ad4e65286d9d28c22a81835293c64
Yuce Tekol <yucetekol@gmail.com>
2016-11-17 03:52:10 +0300

    revamped install script and i3 config; added aptitude

--- install.sh
+++ install.sh
@@ -1,53 +1,43 @@
+#! /bin/bash
+
+set -e
+set -u
+
 ROOT=`pwd`
-BIN_DIR=$HOME/bin
 
-# === install .bashrc ===
-if [ -f $HOME/.bashrc ]
-then
-    mv $HOME/.bashrc $HOME/OLD.bashrc
-fi
-touch $HOME/.bashrc_private
-ln -s $ROOT/shell/_bashrc $HOME/.bashrc
+link_exists () {
+    if [ -L "$1" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-if [ ! -f $BIN_DIR/z.sh ]
-then
-    ln -s $ROOT/bin/z.sh $BIN_DIR/z.sh
-fi
+file_exists () {
+    if [ -f "$1" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-# === install .vimrc ===
-if [ -f $HOME/.vimrc ]
-then
-    mv $HOME/.vimrc $HOME/OLD.vimrc
-fi
-ln -s $ROOT/editor/_vimrc $HOME/.vimrc
+backup_link () {
+    if [ -e "$2" ]; then
+        mv "$2" "$2.OLD"
+    fi
+    ln -s "$1" "$2"
+}
 
-# === install i3 config ===
-i3_config_dir=$HOME/.config/i3
-i3_config_path=$i3_config_dir/config
-if [ -f $i3_config_path ]
-then
-    mv $i3_config_path $i3_config_path.OLD
-fi
-mkdir -p $i3_config_dir
-ln -s  $ROOT/wm/i3/config $i3_config_path
+install () {
+    link_exists "$3" || (echo "Installing $1" && backup_link "$2" "$3")
+}
 
-i3blocks_config_dir=$HOME/.i3
-i3blocks_config_path=$i3blocks_config_dir/i3blocks.conf
-if [ -f $i3blocks_config_path ]
-then
-    mv $i3blocks_config_path $i3blocks_config_path.OLD
-fi
-mkdir -p $i3blocks_config_dir
-ln -s $ROOT/wm/i3/i3blocks.conf $i3blocks_config_path
 
-=== install .Xdefaults ===
-xdefaults_path=$HOME/.Xdefaults
-if [ -f $xdefaults_path ]
-then
-    mv $xdefaults_path $xdefaults_path.OLD
-fi
-ln -s $ROOT/x/Xdefaults $xdefaults_path
+install bashrc    "$ROOT/shell/_bashrc" "$HOME/.bashrc"
+install z         "$ROOT/bin/z.sh" "$HOME/bin/z.sh"
+install vimrc     "$ROOT/editor/_vimrc" "$HOME/.vimrc"
+install i3        "$ROOT/wm/i3/config" "$HOME/.config/i3/config"
+install i3blocks  "$ROOT/wm/i3/i3blocks_`uname -m`" "$HOME/.config/i3/i3blocks"
+install Xdefaults "$ROOT/x/Xdefaults" "$HOME/.Xdefaults"
+install Aptitude  "$ROOT/aptitude/config" "$HOME/.aptitude/config"
 
-# === install fonts ===
-mkdir -p $HOME/.fonts
-cp $ROOT/fonts/* $HOME/.fonts/
