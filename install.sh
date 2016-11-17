454235ee02fb1b0545b587b73c526978a5d47797
Yuce Tekol <yucetekol@gmail.com>
2016-11-17 22:02:25 +0300

    added fzf; added OSX support; cleaned up bashrc

--- install.sh
+++ install.sh
@@ -16,12 +16,21 @@ install () {
     [ -L "$3" ] || (echo "Installing $1" && backup_link "$2" "$3")
 }
 
+mkdir -p $HOME/bin
+
+system=`uname -s`
+system_arch=`uname -s`-`uname -m`
+
+if [ "$system" == "Linux" ]; then
+    mkdir $HOME/.config $HOME/.aptitude
+    install i3        "$ROOT/wm/i3/config"              "$HOME/.config/i3/config"
+    install i3blocks  "$ROOT/wm/i3/i3blocks_`uname -m`" "$HOME/.config/i3/i3blocks"
+    install Xdefaults "$ROOT/x/Xdefaults"               "$HOME/.Xdefaults"
+    install Aptitude  "$ROOT/aptitude/config"           "$HOME/.aptitude/config"
+fi
 
 install bashrc    "$ROOT/shell/_bashrc"             "$HOME/.bashrc"
 install z         "$ROOT/bin/z.sh"                  "$HOME/bin/z.sh"
 install vimrc     "$ROOT/editor/_vimrc"             "$HOME/.vimrc"
-install i3        "$ROOT/wm/i3/config"              "$HOME/.config/i3/config"
-install i3blocks  "$ROOT/wm/i3/i3blocks_`uname -m`" "$HOME/.config/i3/i3blocks"
-install Xdefaults "$ROOT/x/Xdefaults"               "$HOME/.Xdefaults"
-install Aptitude  "$ROOT/aptitude/config"           "$HOME/.aptitude/config"
 install gitignore "$ROOT/git/_gitignore_global"     "$HOME/.gitignore_global"
+install fzf       "$ROOT/bin/fzf-$system_arch"      "$HOME/bin/fzf"
