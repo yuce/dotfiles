5663f2892edc8c3ca33cbea8d5d58c9ca2aa7248
Yuce Tekol <yucetekol@gmail.com>
2016-11-17 23:17:29 +0300

    fixes and enhancements

--- install.sh
+++ install.sh
@@ -22,10 +22,11 @@ system=`uname -s`
 system_arch=`uname -s`-`uname -m`
 
 if [ "$system" == "Linux" ]; then
-    mkdir $HOME/.config $HOME/.aptitude
+    mkdir -p $HOME/.config $HOME/.aptitude
     install i3        "$ROOT/wm/i3/config"              "$HOME/.config/i3/config"
     install i3blocks  "$ROOT/wm/i3/i3blocks_`uname -m`" "$HOME/.config/i3/i3blocks"
     install Xdefaults "$ROOT/x/Xdefaults"               "$HOME/.Xdefaults"
+    install Xmodmap   "$ROOT/x/Xmodmap"                 "$HOME/.Xmodmap"
     install Aptitude  "$ROOT/aptitude/config"           "$HOME/.aptitude/config"
 fi
 
@@ -34,3 +35,4 @@ install z         "$ROOT/bin/z.sh"                  "$HOME/bin/z.sh"
 install vimrc     "$ROOT/editor/_vimrc"             "$HOME/.vimrc"
 install gitignore "$ROOT/git/_gitignore_global"     "$HOME/.gitignore_global"
 install fzf       "$ROOT/bin/fzf-$system_arch"      "$HOME/bin/fzf"
+
